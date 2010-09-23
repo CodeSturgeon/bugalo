b9d07737b2153a9bb5d0dd76fc08929b3ac60539
Nick Fisher <github@nickdafish.com>
2010-09-22 21:20:17 -0400

    Adding optparse and zipping

--- findo.py
+++ findo.py
@@ -4,11 +4,24 @@ import os
 import logging
 from hashlib import md5, sha1
 
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("-s", "--source", dest="source", help="source path")
+parser.add_option("-c", "--chunk-size", dest="size", type="int", default=10,
+                    help="chunk size in MB")
+parser.add_option("-d", "--debug", dest="debug", action="store_true",
+                    default=False, help="turn on debugging")
+(options, args) = parser.parse_args()
+
+
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARN)
 log.addHandler(logging.StreamHandler())
+if options.debug: log.setLevel(logging.DEBUG)
 
 search_path = '/home/flute/for_fish/20100914/kaspT20_2010-09-13/'
+search_path = '/home/flute/for_fish/20100914/'
+
 
 packs = []
 
@@ -46,7 +59,6 @@ def find_packs(path):
             packs.append(make_pack(root))
     return packs
 
-
 def chunkify_sprinkle(no_chunks, fgroup_list):
     chunks = Array([]*no_chunks)
     spin = 0
@@ -70,10 +82,24 @@ def chunkify_fifo(max_size, fgroup_list):
         chunk_size += fg_size
     return chunks
 
-
+import zipfile
 import pprint as pp
 packs = find_packs(search_path)
-chunks = chunkify_fifo((1024*1024),packs)
+chunk_size = (1024*1024) * options.size
+chunks = chunkify_fifo(chunk_size,packs)
+chunk_no = 0
 for chunk in chunks:
     print 'x'*50
-    pp.pprint(chunks)
+    t_size = 0
+    for group in chunk:
+        #pp.pprint(group['total_size'])
+        t_size += group['total_size']
+    print '--',t_size,'%4d%%'%((float(t_size)/float(chunk_size))*100)
+    #zfile = zipfile.ZipFile(os.path.join(search_path,'chunk_%s.zip'%chunk_no),'w')
+    next
+    zfile = zipfile.ZipFile(os.path.join('.','chunk_%s.zip'%chunk_no),'w')
+    for group in chunk:
+        for f in group['files']:
+            zfile.write(os.path.join(group['full_path'],f['name']),os.path.join(group['import_path'],f['name']))
+    zfile.close()
+    chunk_no += 1
