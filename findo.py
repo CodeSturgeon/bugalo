b104b8e0512d35af6105a19278cfa8ce427a3add
Nick Fisher <github@nickdafish.com>
2010-09-22 23:19:09 -0400

    added verbose and no-zip options cleaned up output fixed chunking problems

--- findo.py
+++ findo.py
@@ -10,18 +10,23 @@ parser.add_option("-s", "--source", dest="source", help="source path",
                     default = '/home/flute/for_fish/20100914/')
 parser.add_option("-c", "--chunk-size", dest="size", type="int", default=10,
                     help="chunk size in MB")
+parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+                    default=False, help="turn on info messages")
 parser.add_option("-d", "--debug", dest="debug", action="store_true",
                     default=False, help="turn on debugging")
+parser.add_option("-n", "--no-zip", dest="nozip", action="store_true",
+                    default=False, help="dry run")
 (options, args) = parser.parse_args()
 
 
 log = logging.getLogger()
 log.setLevel(logging.WARN)
 log.addHandler(logging.StreamHandler())
+if options.verbose: log.setLevel(logging.INFO)
 if options.debug: log.setLevel(logging.DEBUG)
 
 search_path = options.source
-
+MB = (1024*1024)
 
 packs = []
 
@@ -74,7 +79,9 @@ def chunkify_fifo(max_size, fgroup_list):
     chunk_size = 0
     for fg in fgroup_list:
         fg_size = int(fg['total_size'])
-        if chunk_size + fg_size > max_size:
+        if fg_size > max_size:
+            log.warn('file group > chunk size - %s'%fg['full_path'])
+        if chunk_size + fg_size > max_size and len(chunk) > 0:
             chunks.append(chunk)
             chunk = []
             chunk_size = 0
@@ -85,21 +92,21 @@ def chunkify_fifo(max_size, fgroup_list):
 import zipfile
 import pprint as pp
 packs = find_packs(search_path)
-chunk_size = (1024*1024) * options.size
+chunk_size = MB * options.size
 chunks = chunkify_fifo(chunk_size,packs)
 chunk_no = 0
 for chunk in chunks:
-    print 'x'*50
+    chunk_no += 1 # Yes, we start at 1
     t_size = 0
     for group in chunk:
-        #pp.pprint(group['total_size'])
         t_size += group['total_size']
-    print '--',t_size,'%4d%%'%((float(t_size)/float(chunk_size))*100)
+    log.info('Chunk %3d - %4.2f MB (%4d%% full)'%(chunk_no, (t_size/MB),
+                                    (float(t_size)/float(chunk_size))*100))
+    if options.nozip: continue
+    t_size.oh_shit
     #zfile = zipfile.ZipFile(os.path.join(search_path,'chunk_%s.zip'%chunk_no),'w')
-    next
-    zfile = zipfile.ZipFile(os.path.join('.','chunk_%s.zip'%chunk_no),'w')
+    zfile = zipfile.ZipFile(os.path.join('.','chunk_%03d.zip'%chunk_no),'w')
     for group in chunk:
         for f in group['files']:
             zfile.write(os.path.join(group['full_path'],f['name']),os.path.join(group['import_path'],f['name']))
     zfile.close()
-    chunk_no += 1
